84ae9b45631d849711344b771028a29437774209
coanh2710 <coanh2710@gmail.com>
2024-12-01 19:29:19 +0700

    update code

--- train.py
+++ train.py
@@ -72,7 +72,7 @@ def main(config,logger, device, seed):
         config, model
     )
     if global_config['torch_compile']:
-        model = torch.compile(model, dynamic=True)
+        model = torch.compile(model, dynamic=True, mode="reduce-overhead")
     device = torch.device("cpu")
     model.to(device)
 

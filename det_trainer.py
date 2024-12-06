14887e6780a5134940d668d85aed07e0cc3d5914
coanh2710 <coanh2710@gmail.com>
2024-12-07 00:04:32 +0700

    update code

--- det_trainer.py
+++ det_trainer.py
@@ -46,11 +46,11 @@ class DetectionModule(L.LightningModule):
     
     def test_step(self, batch, batch_idx):
         output = self.model(batch[0])
-        output = {key:value.cpu().numpy() for key, value in output.items()}
+        output = {key:value.cpu().numpy().astype(np.float32) for key, value in output.items()}
         batch_numpy = []
         batch = [b.cpu() for b in batch]
         for item in batch:
-            batch_numpy.append(item.numpy())
+            batch_numpy.append(item.numpy().astype(np.float32))
         post_result = self.post_process_func(output, batch_numpy[1])
         self.metric_class(post_result, batch_numpy)
         self.log_dict(self.metric_class.get_metric(),prog_bar=True)
